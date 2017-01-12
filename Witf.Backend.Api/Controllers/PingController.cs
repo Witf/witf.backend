3585edddd26a391c37e81613995dc06df605e7c4
Per Svensson <Per Svensson>
2017-01-12 21:48:54 +0100

    convertied .api to web project

---
+++ Witf.Backend.Api/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Witf.Backend.Api.Controllers
+{
+    public class PingController : ApiController
+    {
+        [HttpGet]
+        [Route("api/ping/")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ok", "ok" };
+        }
+    }
+}
--- Witf.Backend.Api/Properties/AssemblyInfo.cs
+++ Witf.Backend.Api/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bd66391c-897b-4f0a-bfd0-e8362012aeea")]
+[assembly: Guid("7a0d1d46-9535-4b91-b108-429046dedec1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
