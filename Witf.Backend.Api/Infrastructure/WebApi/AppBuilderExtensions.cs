d245f0a392a86f3374419ab1787a62ac452bf75c
Marius Ingjer <mariusin79@gmail.com>
2017-01-12 20:49:42 +0100

    lagt til de extensions som michal har laget for å sette opp Auth0

---
+++ Witf.Backend.Api/Infrastructure/WebApi/AppBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿//using Owin;
+
+//namespace Witf.Backend.Api.Infrastructure.WebApi
+//{
+//    public static class AppBuilderExtensions
+//    {
+//        public static IAppBuilder UseAuth0Authentication(this IAppBuilder app)
+//        {
+//            var issuer = $"https://{ConfigurationManager.AppSettings["auth0:Domain"]}/";
+//            var audience = ConfigurationManager.AppSettings["auth0:ClientId"];
+//            var secret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["auth0:ClientSecret"]);
+
+//            var tokenValidationParameters = new TokenValidationParameters
+//            {
+//                RequireSignedTokens = true,
+//                ValidateAudience = true,
+//                ValidateIssuer = true,
+//                RequireExpirationTime = true,
+//                ValidateLifetime = true,
+//                ValidIssuer = issuer,
+//                ValidAudience = audience,
+//                IssuerSigningToken = new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret).SecurityTokens.FirstOrDefault(),
+//            };
+
+//            app.UseJwtBearerAuthentication(
+//                new JwtBearerAuthenticationOptions
+//                {
+//                    AuthenticationMode = AuthenticationMode.Active,
+//                    AuthenticationType = "JWT",
+//                    TokenValidationParameters = tokenValidationParameters
+//                });
+
+//            return app;
+//        }
+
+//    }
+//}
---
+++ Witf.Backend.Api/Infrastructure/WebApi/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Witf.Backend.Api.Infrastructure.WebApi
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserName(this IIdentity identity)
+        {
+            var claimsIdentity = identity as ClaimsIdentity;
+
+            return claimsIdentity?.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
+        }
+    }
+}
