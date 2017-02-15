7151b2c35572b45b65a71e66d187be5651459f1a
Marius Ingjer <mariusin79@gmail.com>
2017-02-15 19:14:24 +0100

    fikset litt mer på patch db scriptet

--- upgrade-db.ps1
+++ upgrade-db.ps1
@@ -1,23 +1,24 @@
 param([switch]$drop, [switch]$integrationTest, $connectionstring)
 
-if ($integrationTest) 
-{
-	[Xml]$xml = Get-Content Witf.Backend.IntegrationTests\App.config
-}
-else 
-{
-	[Xml]$xml = Get-Content Witf.Backend.Api.Selfhost\App.config
-}
+if (-not $connectionstring) {
 
-$m = $xml.configuration.connectionStrings | select-object -first 1 | % { $_.add.connectionString } | sls "Initial Catalog=(?<db>[^;]+)" | select -expand Matches
-$dbName = $m.Groups['db'].Value
+    if ($integrationTest) 
+    {
+    	[Xml]$xml = Get-Content Witf.Backend.IntegrationTests\App.config
+    }
+    else 
+    {
+    	[Xml]$xml = Get-Content Witf.Backend.Api.Selfhost\App.config
+    }
+
+    $m = $xml.configuration.connectionStrings | select-object -first 1 | % { $_.add.connectionString } | sls "Initial Catalog=(?<db>[^;]+)" | select -expand Matches
+    $dbName = $m.Groups['db'].Value
+
+    $connectionstring = "server=(local);database=$dbName;Trusted_Connection=true";
+}
 
 pushd
 set-location Witf.Backend\Database
-
-if (-not $connectionstring) {
-    $connectionstring = "server=(local);database=$dbName;Trusted_Connection=true";
-}
 
 if ($drop) {
     & '..\..\packages\roundhouse.0.8.6\bin\rh.exe' "-c=$connectionstring" '--drop' '--ni'
